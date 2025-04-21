c885f33539e88c98c8d90afaa79dc5c91d497763
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2025-04-21 12:58:56 +0500

    Очистка хуков

--- src/js/sortHeroes.js
+++ src/js/sortHeroes.js
@@ -1,5 +1,6 @@
 console.log("sortHeroes worked");
 
+// Сортировка героев
 export default function sortHeroes(heroes) {
   return [...heroes].sort((item, nextItem) => nextItem.health - item.health); // сортировка по убыванию
 }
