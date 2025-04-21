6acc9c43c43f9792be06edcfbc1dd1d0f4f97d8a
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2025-04-21 15:13:09 +0500

    Решили третью задачу с моком добавили файл src/js/http.js в игнорируемые, так как мы его мокаем

--- src/js/http.js
+++ src/js/http.js
@@ -3,7 +3,8 @@
 
 export default function fetchData(url) {
   if (url === "https://server/user/1") {
-    return { status: "ok", level: 20 };
+    return { status: "ok", level: 20 }; // добавили строку, что бы проходил error:
+    // error  'url' is defined but never used  no-unused-vars
   }
   throw new Error("Mock this!");
 }
