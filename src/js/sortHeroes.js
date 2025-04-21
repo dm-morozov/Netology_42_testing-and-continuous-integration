9143b82f1e57de865a616c662bec03e37dfbe9f1
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2025-04-21 13:15:12 +0500

    Тест Husky с отладкой

--- src/js/sortHeroes.js
+++ src/js/sortHeroes.js
@@ -1,6 +1,6 @@
 console.log("sortHeroes worked");
 
-// Сортировка героев
+// Сортировка героев по здоровью
 export default function sortHeroes(heroes) {
   return [...heroes].sort((item, nextItem) => nextItem.health - item.health); // сортировка по убыванию
 }
